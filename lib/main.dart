20877a286c5cb8761a64e7e201ff5499d8a263ea
IuriKintschev <jtc.iuri07@gmail.com>
2019-10-09 08:41:49 -0400

    feature

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       title: "Flutter's Clothing",
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 111),
+        primaryColor: Color.fromARGB(255, 4, 125, 121),
       ),
       home: HomeScreen(),
     );
